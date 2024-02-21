c741d64d8fac5a60c45e24e6943de561dcfd46b2
peknight <JKpeknight@gmail.com>
2024-02-22 07:48:10 +0800

    update

--- codec-core/shared/src/main/scala/com/peknight/codec/instances/DecoderMigrationInstances.scala
+++ codec-core/shared/src/main/scala/com/peknight/codec/instances/DecoderMigrationInstances.scala
@@ -1,3 +1,21 @@
 package com.peknight.codec.instances
 
-trait DecoderMigrationInstances extends DecoderEitherMigrationInstances
+import cats.Functor
+import cats.data.ValidatedNel
+import cats.syntax.either.*
+import cats.syntax.functor.*
+import cats.syntax.validated.*
+import com.peknight.codec.Decoder
+import com.peknight.generic.migration.Migration
+import com.peknight.generic.priority.MidPriority
+
+trait DecoderMigrationInstances:
+  given migrationDecoder[F[_], T, E, A] (using functor: Functor[F], migration: Migration[F, T, A])
+  : MidPriority[Decoder[F, T, E, A]] =
+    MidPriority {
+      new Decoder[F, T, E, A]:
+        def decode(t: T): F[Either[E, A]] = migration.migrate(t).map(_.asRight[E])
+        def decodeAccumulating(t: T): F[ValidatedNel[E, A]] = migration.migrate(t).map(_.validNel[E])
+    }
+  end migrationDecoder
+end DecoderMigrationInstances
