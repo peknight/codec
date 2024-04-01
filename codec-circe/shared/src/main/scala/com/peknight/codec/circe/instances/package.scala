c56f9a5fe0ee1f614fe6f9a7e94127c1ee2cb20e
peknight <JKpeknight@gmail.com>
2024-04-01 10:58:29 +0800

    optimize

--- codec-circe/shared/src/main/scala/com/peknight/codec/circe/instances/package.scala
+++ codec-circe/shared/src/main/scala/com/peknight/codec/circe/instances/package.scala
@@ -1,7 +1,5 @@
 package com.peknight.codec.circe
 
-import com.peknight.codec.circe.instances.{DecoderDerivationInstances, EncoderDerivationInstances}
-
 package object instances:
   object all extends EncoderDerivationInstances with DecoderDerivationInstances
   object encoder extends EncoderDerivationInstances
