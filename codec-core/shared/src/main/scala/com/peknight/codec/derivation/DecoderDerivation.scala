caff777a8e10ec875d61e21e67e762dd7ad62ad8
peknight <JKpeknight@gmail.com>
2024-08-02 14:15:47 +0800

    fix

--- codec-core/shared/src/main/scala/com/peknight/codec/derivation/DecoderDerivation.scala
+++ codec-core/shared/src/main/scala/com/peknight/codec/derivation/DecoderDerivation.scala
@@ -102,7 +102,7 @@ trait DecoderDerivation:
         val extendedField = configuration.extendedField.contains(label)
         val key = configuration.transformMemberNames(label)
         val current =
-          if extendedField then cursor.remove(labels)(using objectType)
+          if extendedField then cursor.remove(labels.map(configuration.transformMemberNames))(using objectType)
           else cursor.downField(key)(using objectType)
         decoder.decode(current).map(result => defaultOpt
           .filter { _ =>
