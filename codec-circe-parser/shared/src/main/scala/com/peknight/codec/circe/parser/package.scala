fff40c07a8d330db8ddf5f2a6391703c7eca491f
peknight <JKpeknight@gmail.com>
2024-08-16 21:09:49 +0800

    update

--- codec-circe-parser/shared/src/main/scala/com/peknight/codec/circe/parser/package.scala
+++ codec-circe-parser/shared/src/main/scala/com/peknight/codec/circe/parser/package.scala
@@ -1,18 +1,18 @@
-package com.peknight.codec.circe.parser
+package com.peknight.codec.circe
 
 import cats.Applicative
-import cats.syntax.either.*
 import cats.syntax.applicative.*
+import cats.syntax.either.*
+import com.peknight.circe.parser.ext.parse
 import com.peknight.codec.Decoder
 import com.peknight.codec.cursor.Cursor
-import com.peknight.circe.parser.ext.ParserOps.parse
 import com.peknight.codec.error.DecodingFailure
 import io.circe.Json
 
-object ParserOps:
+package object parser:
   def decode[F[_]: Applicative, A](input: String)(using decoder: Decoder[F, Cursor[Json], A])
   : F[Either[DecodingFailure, A]] =
     parse(input) match
       case Left(error) => DecodingFailure(error).asLeft[A].pure[F]
       case Right(json) => decoder.decodeS(json)
-end ParserOps
+end parser
