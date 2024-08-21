1394c0d56dac7fca3dc0ad96eca9cbb163a8c900
peknight <JKpeknight@gmail.com>
2024-08-21 19:44:42 +0800

    update

--- codec-base/shared/src/main/scala/com/peknight/codec/base/Base.scala
+++ codec-base/shared/src/main/scala/com/peknight/codec/base/Base.scala
@@ -11,7 +11,7 @@ trait Base:
   def value: String
   def alphabet: Alphabet
   def decode[F[_]: Applicative]: F[Either[DecodingFailure, ByteVector]]
-  def decodeToBigInt[F[_]: Applicative]: F[Either[DecodingFailure, BigInt]] = decode[F].map(_.map(_.toUnsignedBigInt))
+  def decodeToUnsignedBigInt[F[_]: Applicative]: F[Either[DecodingFailure, BigInt]] = decode[F].map(_.map(_.toUnsignedBigInt))
   def unsafeDecode[F[_]: Applicative]: F[ByteVector] = decode[F].map(_.fold(throw _, identity))
-  def unsafeDecodeToBigInt[F[_]: Applicative]: F[BigInt] = decodeToBigInt[F].map(_.fold(throw _, identity))
+  def unsafeDecodeToUnsignedBigInt[F[_]: Applicative]: F[BigInt] = decodeToUnsignedBigInt[F].map(_.fold(throw _, identity))
 end Base
