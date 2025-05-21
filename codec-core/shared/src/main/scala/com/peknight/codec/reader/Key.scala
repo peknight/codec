8114bdd3bddad712c572bf0cdfbd39e488a09f68
peknight <JKpeknight@gmail.com>
2025-05-21 08:30:06 +0800

    update

--- codec-core/shared/src/main/scala/com/peknight/codec/reader/Key.scala
+++ codec-core/shared/src/main/scala/com/peknight/codec/reader/Key.scala
@@ -5,7 +5,7 @@ import cats.Show
 case class Key(keys: Vector[String])
 object Key:
   val empty: Key = Key(Vector.empty[String])
-  def apply(key: String): Key = Key(Vector(key))
+  def apply(key: String*): Key = Key(key.toVector)
   given showKey: Show[Key] with
     def show(t: Key): String = t.keys.mkString("Key(", ",", ")")
   end showKey
