17a5b81e069ef0fda32e46879571852f58984f3a
peknight <JKpeknight@gmail.com>
2025-06-11 18:42:24 +0800

    update

--- codec-http4s/shared/src/main/scala/com/peknight/codec/http4s/instances/UriInstances.scala
+++ codec-http4s/shared/src/main/scala/com/peknight/codec/http4s/instances/UriInstances.scala
@@ -14,5 +14,14 @@ trait UriInstances:
     )
   given encodeUriS[F[_]: Applicative, S: StringType]: Encoder[F, S, Uri] = Encoder.encodeS[F, S, Uri]
   given decodeUriS[F[_]: Applicative, S: {StringType, Show}]: Decoder[F, Cursor[S], Uri] = Decoder.decodeS[F, S, Uri]
+
+  given stringCodecScheme[F[_]: Applicative]: Codec[F, String, String, Uri.Scheme] =
+    Codec.applicative[F, String, String, Uri.Scheme](_.value)(
+      t => Uri.Scheme.fromString(t).left.map(decodingFailureIsomorphism0[Id](None).from)
+    )
+
+  given encodeSchemeS[F[_]: Applicative, S: StringType]: Encoder[F, S, Uri.Scheme] = Encoder.encodeS[F, S, Uri.Scheme]
+  given decodeSchemeS[F[_]: Applicative, S: {StringType, Show}]: Decoder[F, Cursor[S], Uri.Scheme] =
+    Decoder.decodeS[F, S, Uri.Scheme]
 end UriInstances
 object UriInstances extends UriInstances
