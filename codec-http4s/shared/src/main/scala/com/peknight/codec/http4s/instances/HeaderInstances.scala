126f12dc25803d501c4efb96299e51076388f50b
peknight <JKpeknight@gmail.com>
2025-07-22 11:01:57 +0800

    update

--- codec-http4s/shared/src/main/scala/com/peknight/codec/http4s/instances/HeaderInstances.scala
+++ codec-http4s/shared/src/main/scala/com/peknight/codec/http4s/instances/HeaderInstances.scala
@@ -1,7 +1,9 @@
 package com.peknight.codec.http4s.instances
 
+import cats.{Applicative, Show}
 import com.peknight.codec.cursor.Cursor
 import com.peknight.codec.error.DecodingFailure
+import com.peknight.codec.sum.StringType
 import com.peknight.codec.{Codec, Decoder, Encoder}
 import org.http4s.Header
 
@@ -16,7 +18,7 @@ trait HeaderInstances:
     Encoder.encodeS[F, S, A]
 
   given decodeHeaderS[F[_], S, A, T <: Header.Type](using
-    applicative: Applicative[F], stringType: StringType[S], header: Header[A, T]
+    applicative: Applicative[F], stringType: StringType[S], show: Show[S], header: Header[A, T]
   ): Decoder[F, Cursor[S], A] =
     Decoder.decodeS[F, S, A]
 end HeaderInstances
