fbef6ac3a9e52649a9baaf6e832732dd55567afa
peknight <JKpeknight@gmail.com>
2025-07-22 13:42:03 +0800

    update

--- codec-http4s/shared/src/main/scala/com/peknight/codec/http4s/instances/HeaderInstances.scala
+++ codec-http4s/shared/src/main/scala/com/peknight/codec/http4s/instances/HeaderInstances.scala
@@ -7,6 +7,8 @@ import com.peknight.codec.sum.StringType
 import com.peknight.codec.{Codec, Decoder, Encoder}
 import org.http4s.Header
 
+import scala.reflect.ClassTag
+
 trait HeaderInstances:
   given stringCodecHeader[F[_], A, T <: Header.Type](using applicative: Applicative[F], header: Header[A, T])
   : Codec[F, String, String, A] =
@@ -18,7 +20,7 @@ trait HeaderInstances:
     Encoder.encodeS[F, S, A]
 
   given decodeHeaderS[F[_], S, A, T <: Header.Type](using
-    applicative: Applicative[F], stringType: StringType[S], show: Show[S], header: Header[A, T]
+    applicative: Applicative[F], stringType: StringType[S], show: Show[S], classTag: ClassTag[A], header: Header[A, T]
   ): Decoder[F, Cursor[S], A] =
     Decoder.decodeS[F, S, A]
 end HeaderInstances
